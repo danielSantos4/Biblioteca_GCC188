9b602d219b4a16f262c82c69e85d640917dda831
DanielSantos <danmes12345@gmail.com>
2023-02-24 22:49:54 -0300

    Adicionando diagramas que nao foram e habilitando telaLogin por localhost'

--- Saci/Codigo/servidor.js
+++ Saci/Codigo/servidor.js
@@ -4,11 +4,6 @@ import { fileURLToPath } from 'url';
 import userRouter from "./Routes/user_routes.js"
 const servidor = express()
 
-servidor.use( express.json() )
-servidor.use(express.urlencoded( {extended: true} ))
-servidor.use("/user", userRouter)
-
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -17,8 +12,16 @@ servidor.listen(3000, servico)
 servidor.set('views', path.join(__dirname, 'views'));
 servidor.set('view engine', 'ejs')
 
+servidor.use( express.json() )
+servidor.use(express.urlencoded( {extended: true} ))
+servidor.use("/user", userRouter)
+servidor.get("/", function(req,res) {
+    res.sendFile(path.join(__dirname, "/views/TelaLogin.html"))
+})
+
 function servico(){
     console.log("Servidor rodando ... \n")
 }
 
 console.log(userRouter.stack)
+
